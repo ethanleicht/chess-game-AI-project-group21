21a30c75c8e8b01b10ed99332f5ab20ee65ffab9
ethanleicht <ethanrleicht@gmail.com>
2023-11-01 11:02:03 -0400

    fix errors

--- player/AI.py
+++ player/AI.py
@@ -250,101 +250,103 @@ class AI:
         return arr
 
 
-def calculateb(self, gametiles):
-    material_value = 0
-    positional_value = 0
-    
-    piece_values = {
-        'P': -100, 'N': -320, 'B': -330, 'R': -500, 'Q': -900, 'K': -20000,
-        'p': 100, 'n': 320, 'b': 330, 'r': 500, 'q': 900, 'k': 20000
-    }
+    def calculateb(self, gametiles):
+        material_value = 0
+        positional_value = 0
+        
+        piece_values = {
+            'P': -100, 'N': -320, 'B': -330, 'R': -500, 'Q': -900, 'K': -20000,
+            'p': 100, 'n': 320, 'b': 330, 'r': 500, 'q': 900, 'k': 20000
+        }
 
-    pawn_table = [
-        0, 0, 0, 0, 0, 0, 0, 0,
-        5, 10, 10, -20, -20, 10, 10, 5,
-        5, -5, -10, 0, 0, -10, -5, 5,
-        0, 0, 0, 20, 20, 0, 0, 0,
-        5, 5, 10, 25, 25, 10, 5, 5,
-        10, 10, 20, 30, 30, 20, 10, 10,
-        50, 50, 50, 50, 50, 50, 50, 50,
-        0, 0, 0, 0, 0, 0, 0, 0
-    ]
+        pawn_table = [
+            0, 0, 0, 0, 0, 0, 0, 0,
+            5, 10, 10, -20, -20, 10, 10, 5,
+            5, -5, -10, 0, 0, -10, -5, 5,
+            0, 0, 0, 20, 20, 0, 0, 0,
+            5, 5, 10, 25, 25, 10, 5, 5,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            0, 0, 0, 0, 0, 0, 0, 0
+        ]
 
-    knight_table = [
-        -50, -40, -30, -30, -30, -30, -40, -50,
-        -40, -20, 0, 5, 5, 0, -20, -40,
-        -30, 5, 10, 15, 15, 10, 5, -30,
-        -30, 0, 15, 20, 20, 15, 0, -30,
-        -30, 5, 15, 20, 20, 15, 5, -30,
-        -30, 0, 10, 15, 15, 10, 0, -30,
-        -40, -20, 0, 0, 0, 0, -20, -40,
-        -50, -40, -30, -30, -30, -30, -40, -50
-    ]
+        knight_table = [
+            -50, -40, -30, -30, -30, -30, -40, -50,
+            -40, -20, 0, 5, 5, 0, -20, -40,
+            -30, 5, 10, 15, 15, 10, 5, -30,
+            -30, 0, 15, 20, 20, 15, 0, -30,
+            -30, 5, 15, 20, 20, 15, 5, -30,
+            -30, 0, 10, 15, 15, 10, 0, -30,
+            -40, -20, 0, 0, 0, 0, -20, -40,
+            -50, -40, -30, -30, -30, -30, -40, -50
+        ]
 
-    bishop_table = [
-        -20, -10, -10, -10, -10, -10, -10, -20,
-        -10, 5, 0, 0, 0, 0, 5, -10,
-        -10, 10, 10, 10, 10, 10, 10, -10,
-        -10, 0, 10, 10, 10, 10, 0, -10,
-        -10, 5, 5, 10, 10, 5, 5, -10,
-        -10, 0, 5, 10, 10, 5, 0, -10,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -20, -10, -10, -10, -10, -10, -10, -20
-    ]
-    
-    rook_table = [
-        0, 0, 0, 5, 5, 0, 0, 0,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        5, 10, 10, 10, 10, 10, 10, 5,
-        0, 0, 0, 0, 0, 0, 0, 0
-    ]
-    
-    queen_table = [
-        -20, -10, -10, -5, -5, -10, -10, -20,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -10, 0, 5, 5, 5, 5, 0, -10,
-        -5, 0, 5, 5, 5, 5, 0, -5,
-        0, 0, 5, 5, 5, 5, 0, -5,
-        -10, 5, 5, 5, 5, 5, 0, -10,
-        -10, 0, 5, 0, 0, 0, 0, -10,
-        -20, -10, -10, -5, -5, -10, -10, -20
-    ]
-    # Positional values for kings 
-    king_table = [
-    20, 30, 10, 0, 0, 10, 30, 20,
-    20, 20, 0, 0, 0, 0, 20, 20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30 ]
-    
-    for y in range(8):
-        for x in range(8):
-            piece = gametiles[y][x].pieceonTile.tostring()
-            if piece.islower():
-                positional_value += pawn_table[(7-y)*8+x] if piece == 'p' else 0
-                positional_value += knight_table[(7-y)*8+x] if piece == 'n' else 0
-                positional_value += bishop_table[(7-y)*8+x] if piece == 'b' else 0
-                positional_value += rook_table[(7-y)*8+x] if piece == 'r' else 0
-                positional_value += queen_table[(7-y)*8+x] if piece == 'q' else 0
-                positional_value += king_table[(7-y)*8+x] if piece == 'k' else 0
-            elif piece.isupper():
-                positional_value -= pawn_table[y*8+x] if piece == 'P' else 0
-                positional_value -= knight_table[y*8+x] if piece == 'N' else 0
-                positional_value -= bishop_table[y*8+x] if piece == 'B' else 0
-                positional_value -= rook_table[y*8+x] if piece == 'R' else 0
-                positional_value -= queen_table[y*8+x] if piece == 'Q' else 0
-                positional_value -= king_table[y*8+x] if piece == 'K' else 0
+        bishop_table = [
+            -20, -10, -10, -10, -10, -10, -10, -20,
+            -10, 5, 0, 0, 0, 0, 5, -10,
+            -10, 10, 10, 10, 10, 10, 10, -10,
+            -10, 0, 10, 10, 10, 10, 0, -10,
+            -10, 5, 5, 10, 10, 5, 5, -10,
+            -10, 0, 5, 10, 10, 5, 0, -10,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -20, -10, -10, -10, -10, -10, -10, -20
+        ]
+        
+        rook_table = [
+            0, 0, 0, 5, 5, 0, 0, 0,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            5, 10, 10, 10, 10, 10, 10, 5,
+            0, 0, 0, 0, 0, 0, 0, 0
+        ]
+        
+        queen_table = [
+            -20, -10, -10, -5, -5, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 0, 5, 5, 5, 5, 0, -10,
+            -5, 0, 5, 5, 5, 5, 0, -5,
+            0, 0, 5, 5, 5, 5, 0, -5,
+            -10, 5, 5, 5, 5, 5, 0, -10,
+            -10, 0, 5, 0, 0, 0, 0, -10,
+            -20, -10, -10, -5, -5, -10, -10, -20
+        ]
+        # Positional values for kings 
+        king_table = [
+        20, 30, 10, 0, 0, 10, 30, 20,
+        20, 20, 0, 0, 0, 0, 20, 20,
+        -10, -20, -20, -20, -20, -20, -20, -10,
+        -20, -30, -30, -40, -40, -30, -30, -20,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30 ]
+        
+        for y in range(8):
+            for x in range(8):
+                piece = gametiles[y][x].pieceonTile.tostring()
+                # print(piece)
+                if piece.islower():
+                    positional_value += pawn_table[(7-y)*8+x] if piece == 'p' else 0
+                    positional_value += knight_table[(7-y)*8+x] if piece == 'n' else 0
+                    positional_value += bishop_table[(7-y)*8+x] if piece == 'b' else 0
+                    positional_value += rook_table[(7-y)*8+x] if piece == 'r' else 0
+                    positional_value += queen_table[(7-y)*8+x] if piece == 'q' else 0
+                    positional_value += king_table[(7-y)*8+x] if piece == 'k' else 0
+                elif piece.isupper():
+                    positional_value -= pawn_table[y*8+x] if piece == 'P' else 0
+                    positional_value -= knight_table[y*8+x] if piece == 'N' else 0
+                    positional_value -= bishop_table[y*8+x] if piece == 'B' else 0
+                    positional_value -= rook_table[y*8+x] if piece == 'R' else 0
+                    positional_value -= queen_table[y*8+x] if piece == 'Q' else 0
+                    positional_value -= king_table[y*8+x] if piece == 'K' else 0
 
-            material_value += piece_values[piece]
+                if piece in piece_values:
+                    material_value += piece_values[piece]
 
-    return material_value + positional_value
+        return material_value + positional_value
 
 
 
