b4e4ac99a62567acca7efcd92a1ada2cd601a25f
ralmogezwi <145406131+ralmogezwi@users.noreply.github.com>
2023-11-02 01:08:31 -0400

    Update AI.py

--- player/AI.py
+++ player/AI.py
@@ -590,7 +590,7 @@ class AI:
 
 
         # Adjust the total evaluation based on king safety
-        total_evaluation = material_value + positional_value + center_control_value + pawn_structure_value + tactical_value + endgame_value + king_activity_score + pawn_structure_score + opening_bonus + development_value + king_safety_value
+        total_evaluation = material_value + center_control_value + pawn_structure_value + tactical_value + endgame_value  + pawn_structure_score 
                     
         # Return the negative of the evaluation if playing as black
         return -total_evaluation
